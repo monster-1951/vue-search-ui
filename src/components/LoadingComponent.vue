af54a1eb8119fae4d11d7c1f8406193975775493
consciouscode <consciouscode.dev@gmail.com>
2025-12-28 16:10:43 +0530

    Add loading and empty states with UX safeguards

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,18 @@
+<!-- LoadingComponent.vue -->
+
+<template>
+    <div class="container">
+        Please Wait ⌛....
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+    text-align: center;
+    font-size: x-large;
+    font-weight: 800;
+    display: flex;
+    justify-content: center;
+}
+</style>
---
+++ src/components/NoResults.vue
@@ -0,0 +1,16 @@
+<!-- NoResults.vue -->
+
+<template>
+    <div class="container">
+        No Results Found 👎🏻
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+    text-align: center;
+    font-size: x-large;
+    font-weight: 800;
+}
+</style>
